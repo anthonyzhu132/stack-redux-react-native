ca9e49681b09f740df070a46dee1abe698fc60be
anthonyzhu132 <anthonyzhu132@gmail.com>
2021-03-22 22:50:25 -0700

    added functionality to ListItem to render out the titles of all of the libraries that were passed in as props from LibraryList

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native;'
+import CardItem from './common/CardItem';
 
 class ListItem extends Component {
   render() {
-    
+    return (
+      <CardIem>
+        <Text>
+          {this.props.library.title}
+        </Text>
+      </CardIem>
+    )
   }
 }
 
